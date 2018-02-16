f18ca964cfa5f61e543f75709fb96bb06af15056
Tom Richards <tom@tomrichards.net>
2018-02-16 13:23:54 -0500

    Remove content files, add decode specs

--- spec/decode_spec.cr
+++ spec/decode_spec.cr
@@ -3,23 +3,25 @@ require "logger"
 
 describe BEncoding do
   it "decodes strings" do
-    decoded = BEncoding.decode("7:foo bar")
-    decoded.should eq("foo bar")
+    BEncoding.decode("7:foo bar").should eq("foo bar")
+    BEncoding.decode("0:").should eq("")
+    BEncoding.decode("5:café").should eq("café")
   end
 
   it "decodes integers" do
-    decoded = BEncoding.decode("i42e")
-    decoded.should eq(42)
+    BEncoding.decode("i42e").should eq(42)
+    BEncoding.decode("i0e").should eq(0)
+    BEncoding.decode("i-42e").should eq(-42)
   end
 
   it "decodes lists" do
-    decoded = BEncoding.decode("li1ei2ei3ee")
-    decoded.should eq([1, 2, 3])
+    BEncoding.decode("li1ei2ei3ee").should eq([1, 2, 3])
+    BEncoding.decode("l6:你好6:中文e").should eq(["你好", "中文"])
   end
 
   it "decodes dictionaries" do
-    decoded = BEncoding.decode("d3:bari-10e3:fooi1ee")
-    decoded.should eq({"foo" => 1, "bar" => -10})
+    BEncoding.decode("d3:bari-10e3:fooi1ee").should eq({"foo" => 1, "bar" => -10})
+    BEncoding.decode("d3:foo3:bar3:baz3:quxe").should eq({"foo" => "bar", "baz" => "qux"})
   end
 
   fixtures_logger = Logger.new(File.new("#{__DIR__}/fixtures.log", "w"))
--- src/bencoding/version.cr
+++ src/bencoding/version.cr
@@ -1,3 +1,3 @@
 module BEncoding
-  VERSION = "0.1.0"
+  VERSION = "1.0.0"
 end
